b044b86df080477b9a5d580c378c6dd9037f5545
sirlilpanda <sirlilpanda@gmail.com>
2022-09-03 01:22:47 +1200

    added and replace html with components

--- lib/site_web/views/projects_view.ex
+++ lib/site_web/views/projects_view.ex
@@ -1,3 +1,18 @@
 defmodule SiteWeb.ProjectsView do
   use SiteWeb, :view
+
+
+  def project_card(assigns, title, image_path, project_path) do
+  ~H"""
+    <li class="project-li"><a class="group project-aref" href={"#{project_path}"}>
+            <img class="project-img group-hover:project-img-hov rounded-xl" src={"#{image_path}"}>
+            <p1 class="project-p1 group-hover:project-p1-hov"><%= title%></p1>
+        </a>
+    </li>
+  """
+
+
+  end
+
+
 end

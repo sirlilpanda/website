317a1add56d5b7c26f981959742b54b606269eb9
sirlilpanda <sirlilpanda@gmail.com>
2022-09-03 01:27:30 +1200

    added and replace html with components

--- lib/site_web/views/layout_view.ex
+++ lib/site_web/views/layout_view.ex
@@ -4,4 +4,15 @@ defmodule SiteWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def nav_bar_element(assigns, href, title) do
+    ~H"""
+      <li class="navli group">
+        <div class="navdiv group-hover:bg-black group-hover:w-full transition-all duration-200">
+        </div>
+        <a class="nava group-hover:text-black" href={"#{href}"}><%= title %></a>
+      </li>
+    """
+  end
+
 end

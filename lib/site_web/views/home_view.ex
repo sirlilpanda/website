cd6ac5d12897bd3ab11625c213b959fc0213c7f5
sirlilpanda <sirlilpanda@gmail.com>
2022-09-08 16:44:24 +1200

    changed some css

--- lib/site_web/views/home_view.ex
+++ lib/site_web/views/home_view.ex
@@ -4,11 +4,11 @@ defmodule SiteWeb.HomeView do
   def gen_skill_card(assigns, image_path, title, skill_level, disc_l1, disc_l2, disc_l3) do
     ~H"""
       <div class="tech-container">
-          <div class="bg-zinc-800 rounded-2xl pr-40 py-2 w-[1000px]">
+          <div class="bg-zinc-800 rounded-2xl py-2 w-[1000px] shadow-inner">
               <img src={"#{image_path}"} class="max-h-28 float-left px-5"/>
               <h1 class="tech-header float-left pt-1"><%= title %>:</h1>
 
-              <div class="mt-1 float-right rounded-full bg-lime-600 px-10 w-[570px]">
+              <div class="mt-1 float-right rounded-full bg-lime-600 px-10 w-[570px] shadow-lg mr-20">
                   <p class="home-p"><%= skill_level %></p>
               </div>
               <div class="mt-10">

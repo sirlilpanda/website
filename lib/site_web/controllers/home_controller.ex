54ee7f8d4336f2970d3a346b86390777e51fa640
sirlilpanda <sirlilpanda@gmail.com>
2022-09-08 16:37:06 +1200

    added post function

--- lib/site_web/controllers/home_controller.ex
+++ lib/site_web/controllers/home_controller.ex
@@ -1,7 +1,14 @@
+import IO
+
 defmodule SiteWeb.HomeController do
   use SiteWeb, :controller
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def post(conn, params) do
+    IO.inspect(params)
+  end
+
 end
